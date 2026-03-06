b46a75724ec62167baf5f34c1abc11af56b50573
HenrikLeutscher <HenrikHezo@hotmail.com>
2026-03-06 23:58:36 +0100

    Updated AddToCart component + made ContinueShopping Component

--- src/app/components/AddToCartButton.tsx
+++ src/app/components/AddToCartButton.tsx
@@ -4,27 +4,33 @@ import { useState } from "react";
 import type { Product } from "../types/product";
 import { useCartStore } from "../stores/cartStore";
 
-export default function AddToCartButton({ product }: { product: Product}) {
-    const addItem = useCartStore((s) => s.addItem);
-    const [message, setMessage] = useState("");
+export default function AddToCartButton({ product }: { product: Product }) {
+  const addItem = useCartStore((s) => s.addItem);
+  const [message, setMessage] = useState("");
 
-    function showMessage(text: string) {
-        setMessage(text);
-        window.setTimeout(() => setMessage(""), 1500);
-    }
+  function showMessage(text: string) {
+    setMessage(text);
+    window.setTimeout(() => setMessage(""), 1500);
+  }
 
-    return (
-        <div className="mt-2">
-            <button
-            type="button"
-            onClick={() => {
-                addItem(product);
-                showMessage("Product Added to cart");
-            }} className="bg-blue-500 text-white px-4 py-2 rounded">
-                Add to Cart
-            </button>
+  return (
+    <div className="mt-2">
+      <button
+        type="button"
+        onClick={() => {
+          addItem(product);
+          showMessage("Product Added to cart");
+        }}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Add to Cart
+      </button>
 
-            { message && <div className="mt-2 text-sm text-gray-600">{message}</div>}
+      {message && (
+        <div className="mt-2 text-sm text-white bg-green-500 px-2 py-1 rounded w-1/3">
+          {message}
         </div>
-    )
+      )}
+    </div>
+  );
 }
